18bdf73a34b794b4a6972ef48cb32d4594947873
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-04-30 13:12:56 +0530

    All are added

---
+++ src/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white "
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
+
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/82665255-220c-4a6b-bdd0-737a92cc671a"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white w-fit px-6 py-2 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-red-500 cursor-pointer duration-200 hover:scale-110 hover:bg-gradient-to-r hover:from-red-500 hover:to-blue-500 ">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
